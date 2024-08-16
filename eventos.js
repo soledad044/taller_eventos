f8e73f55158d69edd71214c42f36fa592d6bfb27
Soledad044 <adm.olivera95@gmail.com>
2024-08-16 14:16:42 -0300

    "Solución para evitar que el evento click del botón dispare el evento click del div"

--- eventos.js
+++ eventos.js
@@ -5,3 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Hola! Soy el div');
     });
 });
+
+
+//** parte 5 de la actividad */
+
+// Seleccionamos el botón dentro del div
+const boton = buttonContainer.querySelector('button');
+
+// Añadimos un método para evitar que el click en el botón se propague al div.
+boton.addEventListener('click', function(event) {
+    alert('¡Botón clickeado!');
+    event.stopPropagation(); // aquí  
+});
